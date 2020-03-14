6a2ba010696d64a55d3f0a9f744dbfc6faaf0bc3
taka-hoop <takanari_gal1318@ezweb.ne.jp>
2020-03-14 16:46:21 +0900

    Initial commit

---
+++ main.js
@@ -0,0 +1,21 @@
+// document.querySelector('button').addEventListener('click', () =>{
+//   const li = document.createElement('li');
+//   const text = document.querySelector('input');
+//   li.textContent = text.value;
+//   document.querySelector('ul').appendChild(li);
+
+//   for(let text.value = 1; text.value <= )
+
+  
+
+for(let i = 1; i <= 30; i++){
+  if(i % 3 === 0 && i % 5 === 0) {
+    console.log(`${i} Fizzbuzz`)
+  } else if (i % 3 === 0) {
+    console.log(`${i}Fizz`)
+  } else if (i % 5 === 0) {
+    console.log(`${i}Buzz`)
+  } else {
+    console.log(i)
+  }
+}
